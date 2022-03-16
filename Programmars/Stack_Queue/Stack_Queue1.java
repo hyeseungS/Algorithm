6e5df83a58c84dce335617c83e5ea5f006ea4c3f
hyeseungS <hyeseung2000@gmail.com>
2022-03-16 15:54:46 +0900

    Add : Stack_Queue - 기능개발

--- Programmars/Stack_Queue/Stack_Queue1.java
+++ Programmars/Stack_Queue/Stack_Queue1.java
@@ -1,24 +1,36 @@
-import java.util.HashMap;
+import java.util.*;
 class Solution {
-    public String solution(String[] participant, String[] completion) {
-        String answer = "";
-        HashMap<String, Integer> hm = new HashMap();
-        for(String name : participant) {
-            if(hm.containsKey(name))
-                hm.put(name, hm.get(name)+1);
-            else
-                hm.put(name, 1);
+    public List<Integer> solution(int[] progresses, int[] speeds) {
+        Queue<Integer> queue = new LinkedList<>();
+        List<Integer> answer = new ArrayList<>();
+        int progress = 0; // 먼저 배포되어야 하는 기능의 남은 작업 일수
+        int count = 0; // 배포 개수
+        
+        // 각 기능의 남은 작업 일수 큐에 저장
+        for(int i = 0; i < progresses.length; i++) {
+            int value = (100-progresses[i])/speeds[i];
+            int remainder = (100-progresses[i])%speeds[i];
+            if(remainder > 0) value++;
+            queue.add(value);
         }
-        for(String name : completion) {
-            if(hm.get(name) == 1)
-                hm.remove(name);
-            else if(hm.get(name) > 1)
-                hm.replace(name, hm.get(name)-1);
+        
+        // 각 배포마다 배포되는 기능의 개수
+        for(int i = 0; i < progresses.length; i++) {
+            count++;
+            
+            if(count == 1) // 먼저 배포되어야하는 작업
+                progress = queue.poll();
+            else 
+                queue.poll();
+            
+            if(queue.peek() != null && progress >= queue.peek()) {
+                continue;
+            }
+            
+            answer.add(count);
+            count = 0;
         }
-        for (String name : participant) { 
-            if(hm.containsKey(name))
-                answer = name;
-        }
+        
         return answer;
     }
 }
