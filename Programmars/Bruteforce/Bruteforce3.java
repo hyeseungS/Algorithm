4edb75ad74e1ff3ec3d90ac0f088cae9fb85d334
hyeseungS <hyeseung2000@gmail.com>
2022-04-06 20:33:10 +0900

    Update : Bruteforce3 - 카펫

--- Programmars/Bruteforce/Bruteforce3.java
+++ Programmars/Bruteforce/Bruteforce3.java
@@ -1,17 +1,13 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
-        int total  = 0;
+        int total  = brown + yellow; // 전체 격자 개수
         for(int i = 3; i < 2000; i++) {
-            for(int j = 3; j <= i; j++) {
-                // 전체 격자 개수
-                total = i * j;
-                // 찾으면 반복문 나옴
-                if((total - (i-2)*(j-2)) == brown && (i-2)*(j-2) == yellow) {
-                    answer[0] = i;
-                    answer[1] = j;
-                    break;
-                }
+            // 찾으면 반복문 나옴
+            if((total - (i-2)*(total/i-2)) == brown && (i-2)*(total/i-2) == yellow) {
+                answer[0] = total/i;
+                answer[1] = i;
+                break;
             }
         }
         return answer;
