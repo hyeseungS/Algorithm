7292ecfaa042d4eb11e682dba7ddf29cd77d7210
hyeseungS <hyeseung2000@gmail.com>
2022-05-02 03:12:35 +0900

    Add : DFS_BFS1 - 타겟넘버

---
+++ Programmars/DFS_BFS/DFS_BFS1.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Solution {
+    int answer = 0;
+    public int solution(int[] numbers, int target) {
+        dfs(numbers, target, 0, 0);
+        return answer;
+        
+    }
+    // dfs 
+    void dfs(int[] numbers, int target, int depth, int sum) {
+        if(depth == numbers.length) {
+            if(sum == target)
+                answer++;
+            return;
+        }
+        dfs(numbers, target, depth + 1, sum + numbers[depth]);
+        dfs(numbers, target, depth + 1, sum - numbers[depth]);
+    }   
+
+}
