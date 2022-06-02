6edfe2bad501cb1ada1158abcd34055070f16d93
신혜승 <hyeseung2000@gmail.com>
2022-06-02 21:16:33 +0900

    Greedy5 - 섬 연결하기

--- Programmars/Greedy/Greedy5.java
+++ Programmars/Greedy/Greedy5.java
@@ -1,31 +1,73 @@
+import java.util.*;
 class Solution {
-    public int solution(int n, int[] lost, int[] reserve) {
-        int answer = lost.length;
-        // 여분의 체육복을 가져왔지만 체육복을 잃어버린 학생
-        for(int l = 0; l < lost.length; l++){
-            for(int j = 0; j < reserve.length; j++){
-                if(lost[i] == reserve[j]){
-                    lost[i] = reserve[j] = -1;
-                    answer--;
-                    // break;
-                }
-            }
+    public static class Island implements Comparable<Island>{
+        int from ;
+        int to;
+        int weight;
+        
+        public Island(int from, int to, int weight){
+            this.from = from;
+            this.to = to;
+            this.weight = weight;
         }
-        System.out.println(answer);
-        // 1 , 2, 3, 4, 5  /  1,2, 3, 5,6, 7, 8
-        // -1, +1했을시 같다면 체육복 빌려줄 수 있음
-        for(int i=0; i<reserve.length; i++){
-            if(reserve[i] == -1) continue;
-            for(int j=0; j<lost.length; j++){
-                if(lost[j] == -1) continue;
-                if(reserve[i] - 1 == lost[j] || reserve[i] + 1 == lost[j]){
-                    reserve[i] = lost[j] = -1;
-                    answer--;
-                    break;
-                }
-            }
+        public int compareTo(Island o){
+            return this.weight - o.weight;
+        }        
+        
+    }
+    
+    static int[] parent;
+    static PriorityQueue<Island> pq ;
+    public int solution(int n, int[][] costs) {
+        int answer = 0;
+        parent = new int[n];
+        for( int i=0; i<n; i++){
+            parent[i] = i;
         }
-        System.out.println(answer);
-        return n - answer;
+        pq = new PriorityQueue<>();
+        
+        for( int i=0; i<costs.length; i++){
+            pq.add(new Island(costs[i][0], costs[i][1], costs[i][2] ));
+        }
+        
+        // 크루스칼 algo
+        int V =0 ; // 간선 수 
+        while( !pq.isEmpty()){
+            Island cur = pq.poll();
+            int x = cur.from;
+            int y = cur.to;
+            int weight = cur.weight;
+            
+            // from - to 연결 안되어있다면 연결 -> answer ++ 
+            if ( union(x,y) ){
+                answer += weight;
+                V ++;
+            }
+            if( V == n-1) break;
+            
+        }
+        
+        return answer;
+    }
+    private static boolean union(int x, int y){
+        int px = getParent(x);
+        int py = getParent(y);
+        
+        if ( px == py ) return false;
+        else{
+            // 작은 곳에 붙이기
+            if( px < py ) {
+                parent[py] = px;
+            }
+            else {
+                parent[px] = py;
+            }
+            return true;
+        }
+        
+    }
+    private static int getParent(int x){
+        if( parent[x] == x ) return x;
+        else return parent[x] = getParent( parent[x] );
     }
 }
