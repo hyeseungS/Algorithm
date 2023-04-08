196f81e480c35829c8392658c575edbd86bdc28c
신혜승 <hyeseung2000@gmail.com>
2023-04-08 17:12:08 +0900

    Add: 10952 - A+B - 5

---
+++ Baekjoon/반복문/10952.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        
+        while(true) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            
+            if(a == 0 && b == 0)
+                break;
+
+            
+            bw.write((a + b) + "\n");
+        }
+        
+        bw.close();
+    }
+}
