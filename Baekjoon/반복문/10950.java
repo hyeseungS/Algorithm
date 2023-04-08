b750da604d243617e87ee5056df7b03c08d78810
신혜승 <hyeseung2000@gmail.com>
2023-04-08 16:46:48 +0900

    Add: 15552 - 빠른 A+B

--- Baekjoon/반복문/10950.java
+++ Baekjoon/반복문/10950.java
@@ -4,15 +4,10 @@ class Main {
 	public static void main (String[] args) throws java.lang.Exception {
 	    Scanner in = new Scanner(System.in);
 		int t = in.nextInt();
-        int[] result = new int[t];
 		for(int i = 0; i < t; i++) {
 		    int a = in.nextInt();
 		    int b = in.nextInt();
-		    result[i] = a + b;
-		}
-		
-		for(int i = 0; i < t; i++) {
-		    System.out.println(result[i]);
+		    System.out.println(a + b);
 		}
 	}
 }
