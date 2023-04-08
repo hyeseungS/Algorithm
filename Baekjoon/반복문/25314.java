04584af72737290e0451f99e3bd90e904049d8e0
신혜승 <hyeseung2000@gmail.com>
2023-04-08 16:33:15 +0900

    Add: 25314 - 코딩은 체육과목입니다

---
+++ Baekjoon/반복문/25314.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Main {
+	public static void main (String[] args) throws java.lang.Exception {
+	    Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		
+		int result = 0;
+		
+		for(int i = 0; i < n/4; i++) {
+		    System.out.print("long ");
+		}
+		
+		System.out.print("int");
+		
+	}
+}
