2f608ade684400bf4634e0d3008fb04e056bba39
신혜승 <hyeseung2000@gmail.com>
2023-04-14 18:26:14 +0900

    Add: 11654 - 아스키 코드

---
+++ Baekjoon/문자열/11654.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+class Main {
+	public static void main (String[] args) throws IOException {
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	    
+	    bw.write((int) br.readLine().charAt(0) + "");
+	    
+	    bw.close();
+	}
+}
