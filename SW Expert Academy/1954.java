bf25b3a64365553ec6981914c9ce1aa12d2bc784
신혜승 <hyeseung2000@gmail.com>
2023-05-15 17:57:53 +0900

    Update: 1954 - 달팽이 숫자

--- SW Expert Academy/1954.java
+++ SW Expert Academy/1954.java
@@ -1,44 +1,45 @@
-import java.util.Scanner;
-import java.io.FileInputStream;
+import java.io.*;
 
 class Solution
 {
-    static int[] dr = { 0, 1, 0, -1 }; // 우하좌상 순서
-	static int[] dc = { 1, 0, -1, 0 };
     
-	public static void main(String args[]) throws Exception
+    // y 1만큼 증가, x 1만큼 증가, y 1만큼 감소, x 1만큼 감소 순서
+    static int[] dx = {0, 1, 0, -1};
+    static int[] dy = {1, 0, -1, 0};
+        
+	public static void main(String args[]) throws IOException
 	{
-		Scanner sc = new Scanner(System.in);
-		int T;
-		T=sc.nextInt();
-
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+        int T = Integer.parseInt(br.readLine());
+        
 		for(int test_case = 1; test_case <= T; test_case++)
 		{
-			int n = sc.nextInt();
+            int n = Integer.parseInt(br.readLine());
             int[][] snail = new int[n][n];
-            int dir = 0; 
-			int x = 0, y = 0;
+            int x = 0, y = 0;
+            int d = 0;
+            
             for(int i = 1; i <= n * n; i++) {
-			// x값 1 증가
-            // y값 1 증가
-            // x값 1 감소
-            // y값 1 감소
-            // x값 1 증가
                 snail[x][y] = i;
-				if (x + dr[dir] >= n || x + dr[dir] < 0 || y + dc[dir] >= n || y + dc[dir] < 0
-						|| snail[x + dr[dir]][y + dc[dir]] != 0) {
-					// 배열을 벗어나는지 //그 자리에 숫자 있는지 확인
-					dir = (dir + 1) % 4; // 방향전환
-				}
-				x += dr[dir];
-				y += dc[dir];
+            	if(x + dx[d] < 0 || x + dx[d] >= n || y + dy[d] < 0 || y + dy[d] >= n 
+                   || snail[x + dx[d]][y + dy[d]] != 0) {
+                	d = (d + 1) % 4; // 방향 전환
+                }
+               	x += dx[d];
+                y += dy[d];
             }
-            System.out.println("#" + test_case);
-			for (int i = 0; i < n; i++) {
-				for (int s : snail[i])
-					System.out.print(s + " ");
-				System.out.println();
-			}
+            
+            bw.write("#" + test_case + "\n");
+            for(int i = 0; i < n; i++) {
+                for(int j = 0; j < n; j++) {
+                	bw.write(snail[i][j] + " ");
+                }
+                bw.write("\n");
+            }
 		}
+        
+        bw.close();
 	}
 }
