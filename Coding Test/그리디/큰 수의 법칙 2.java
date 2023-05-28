c4b1a1ccfddecfedee14a01338b5709f39ee41c5
신혜승 <hyeseung2000@gmail.com>
2023-05-28 20:41:44 +0900

    Add : 그리디 - 큰 수의 법칙 2

--- Coding Test/그리디/큰 수의 법칙 2.java
+++ Coding Test/그리디/큰 수의 법칙 2.java
@@ -7,17 +7,25 @@ class Main {
 	    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 	    StringTokenizer st = new StringTokenizer(br.readLine());
 	    
+	    // N, M, K를 공백으로 구분하여 입력받기
 	    int N = Integer.parseInt(st.nextToken());
+	    int M = Integer.parseInt(st.nextToken());
 	    int K = Integer.parseInt(st.nextToken());
 	    
-	    int ans = 0;
+	    st = new StringTokenizer(br.readLine());
 	    
-	    while(N != 1) {
-	        if(N % K == 0) N /= K;
-	        else N--;
-	        ans++;
+	    // N개 수를 공백으로 구분하여 입력받기
+	    int[] arr = new int[N];
+	    for(int i = 0; i < N; i++) {
+	        arr[i] = Integer.parseInt(st.nextToken());
 	    }
 	    
+	    // 입력 받은 수들 정렬하기
+	    Arrays.sort(arr);
+	    
+	    int ans = (M / (K + 1)) * (K * arr[N - 1] + arr[N - 2]) + (M % (K + 1)) * arr[N - 1];
+	    
+	    // 최종 답안 출력
 	    bw.write(ans + "");
 	    bw.close();
 	}
