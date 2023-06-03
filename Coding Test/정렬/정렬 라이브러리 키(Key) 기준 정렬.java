9dc0ab70380f28066cc2486eea95660d0167ddf6
신혜승 <hyeseung2000@gmail.com>
2023-06-03 21:06:02 +0900

    Add : 정렬 라이브러리 키(Key) 기준 정렬

--- Coding Test/정렬/정렬 라이브러리 키(Key) 기준 정렬.java
+++ Coding Test/정렬/정렬 라이브러리 키(Key) 기준 정렬.java
@@ -1,16 +1,46 @@
 import java.util.*;
 
+class Fruit implements Comparable<Fruit> {
+
+    private String name;
+    private int key;
+
+    public Fruit(String name, int key) {
+        this.name = name;
+        this.key = key;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getKey() {
+        return this.key;
+    }
+
+    // 정렬 기준은 '점수가 낮은 순서'
+    @Override
+    public int compareTo(Fruit other) {
+        if (this.key < other.key) {
+            return -1;
+        }
+        return 1;
+    }
+}
+
 public class Main {
 
     public static void main(String[] args) {
-    	
-        int[] arr = {7, 5, 9, 0, 3, 1, 6, 2, 4, 8};
+        List<Fruit> fruits = new ArrayList<>();
 
-        Arrays.sort(arr);
+        fruits.add(new Fruit("바나나", 2));
+        fruits.add(new Fruit("사과", 5));
+        fruits.add(new Fruit("당근", 3));
 
-        for(int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
+        Collections.sort(fruits);
+
+        for (int i = 0; i < fruits.size(); i++) {
+            System.out.print("(" + fruits.get(i).getName() + "," + fruits.get(i).getKey() + ") ");
         }
     }
-
 }
