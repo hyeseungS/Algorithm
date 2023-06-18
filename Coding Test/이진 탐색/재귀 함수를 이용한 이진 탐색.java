fd774744fc1c3cf833c8c78195423db1e39a03e1
신혜승 <hyeseung2000@gmail.com>
2023-06-18 18:28:20 +0900

    Add : 이진 탐색 - 재귀 함수를 이용한 이진 탐색

--- Coding Test/이진 탐색/재귀 함수를 이용한 이진 탐색.java
+++ Coding Test/이진 탐색/재귀 함수를 이용한 이진 탐색.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.io.*;
 
-class Solution {
+class Main {
 	public static void main (String[] args) throws IOException {
 	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
