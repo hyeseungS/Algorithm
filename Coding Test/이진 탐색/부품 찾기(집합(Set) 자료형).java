658b2c24b1c23fc78cba2578ff1afb78a81ed5db
신혜승 <hyeseung2000@gmail.com>
2023-06-18 19:43:10 +0900

    Add : 이진 탐색 - 부품 찾기(집합(Set) 자료형

--- Coding Test/이진 탐색/부품 찾기(집합(Set) 자료형).java
+++ Coding Test/이진 탐색/부품 찾기(집합(Set) 자료형).java
@@ -7,40 +7,29 @@ class Main {
 	    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 	    StringTokenizer st;
 	    
+	    int N = Integer.parseInt(br.readLine()); // 부품의 개수
+	    // 부품 번호 입력 받기
 	    st = new StringTokenizer(br.readLine());
+	    // 집합(Set) 정보를 처리하기 위한 HashSet 라이브러리
+	    HashSet<Integer> s = new HashSet<>();
+	    for(int i = 0; i < N; i++) 
+	        s.add(Integer.parseInt(st.nextToken()));
 	    
-	    int n = Integer.parseInt(st.nextToken()); // 원소의 개수
-	    int target = Integer.parseInt(st.nextToken()); // 찾고자 하는 값
-	    
-	    // 전체 원소 입력 받기
+	    int M = Integer.parseInt(br.readLine()); // 문의한 부품 개수
+	    // 문의한 부품 번호 입력 받기
 	    st = new StringTokenizer(br.readLine());
-	    
-	    int[] array = new int[n];
-	    for(int i = 0; i < n; i++) {
-	        array[i] = Integer.parseInt(st.nextToken());
+	    int[] targets = new int[M];
+
+	    for(int i = 0; i < M; i++) {
+	    	targets[i] = Integer.parseInt(st.nextToken());
 	    }
-	    
-	    // 이진 탐색 수행 결과 출력
-	    int result = binarySearch(array, target, 0, n - 1);
-	    if(result == -1) {
-	    	bw.write("원소가 존재하지 않습니다.");
+	    // 손님이 확인 요청한 부품 번호를 하나씩 확인
+	    for(int i = 0; i < M; i++) {
+	    	// 해당 부품 존재하는지 확인
+	        if(s.contains(targets[i])) bw.write("yes "); 
+	        else bw.write("no ");
 	    }
-	    else bw.write(result + 1 + "");
 	    
 	    bw.close();
-	}
-	
-	// 이진 탐색 소스코드 구현(재귀 함수)
-	public static int binarySearch(int[] array, int target, int start, int end) {
-	    if(start > end) {
-	        return -1;
-	    }
-	    int mid = (start + end) / 2;
-	    // 찾은 경우 중간점 인덱스 반환
-	    if(array[mid] == target) return mid;
-	    // 중간점의 값보다 찾고자 하는 값이 작은 경우 왼쪽 확인
-	    else if(array[mid] > target) return binarySearch(array, target, start, mid - 1);
-	    // 중간점의 값보다 찾고자 하는 값이 큰 경우 오른쪽 확인
-	    else return binarySearch(array, target, mid + 1, end);
 	}
 }
